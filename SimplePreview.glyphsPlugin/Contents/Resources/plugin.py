23e9f3d88482660a20464901538678388e5641b8
Daekwon Kim <nanife@live.com>
2021-02-23 02:38:11 +0900

    v2.0

--- SimplePreview.glyphsPlugin/Contents/Resources/plugin.py
+++ SimplePreview.glyphsPlugin/Contents/Resources/plugin.py
@@ -1,4 +1,11 @@
 # encoding: utf-8
+from __future__ import division, print_function, unicode_literals
+import objc
+from GlyphsApp import *
 from GlyphsApp.plugins import *
+import sys
 
-import SimplePreview
+if sys.version[0] == '2':
+	import SimplePreview
+else:
+	import LoadSP3
